811848bf1f2c5e0024bf1ec75d18fbb7c84c3467
Johan Berntsson <johanberntsson@users.noreply.github.com>
2020-05-16 00:27:28 +0900

    Added scope stubs

--- lib/scope.h
+++ lib/scope.h
@@ -1,4 +1,12 @@
 ! Routines to update what is in scope
+! http://www.inform-fiction.org/manual/html/s32.html#p244
+!
+! add_to_scoe
+!
+! scope_stage:
+! 1: allow multiple objects or not (true, false)
+! 2: use ScopeWithin an PlaceInScope to add objects to scope
+! 3: write an error message
 
 Constant MAX_SCOPE = 32;
 
@@ -32,31 +40,59 @@ Array scope-->MAX_SCOPE;
 	_SearchScope(child(p_start_pos));
 ];
 
-[ InScope; ! TODO
+
+[ SearchScope p_domain1 p_domain2 p_context _i;
+	! SearchScope  domain1 domain2 context
+	! Works out what objects are in scope (possibly asking an outside
+	! routine), but does not look at anything the player has typed.
 ];
 
-[ LoopOverScope; ! TODO
+[ AddToScope p_obj;
+	! according to DM this can be called from add_to_scope
 ];
 
-[ PlaceInScope; ! TODO
+[ InScope p_obj;
+	! Used in “scope routines” (only) when scope_stage is set to 2 (only).
+	! Places obj in scope for the token currently being parsed. No other
+	! objects are placed in scope as a result of this, unlike the case of
+	! ScopeWithin. No return value
 ];
 
-[ ScopeWithin; ! TODO
+[ LoopOverScope p_routine p_actor;
+	! Calls routine p_routine(obj) for each object obj in scope for the
+	! given actor. If no actor is given, the actor defaults to be the player.
+	! No return value
 ];
 
-[ TestScope; ! TODO
+[ PlaceInScope p_obj;
+	! Used in “scope routines” (only) when scope_stage is set to 2 (only).
+	! Places obj in scope for the token currently being parsed. No other
+	! objects are placed in scope as a result of this, unlike the case of
+	! ScopeWithin. No return value
 ];
 
-[ ObjectIsUntouchable item flag1 flag2 ancestor i;
-    ! Determine if there's any barrier preventing the player from moving
-    ! things to "item".  Return false if no barrier; otherwise print a
-    ! suitable message and return true.
-    ! If flag1 is set, do not print any message.
-    ! If flag2 is set, also apply Take/Remove restrictions.
+[ ScopeWithin p_obj;
+	! Used in “scope routines” (only) when scope_stage is set to 2 (only).
+	! Places the contents of obj in scope for the token currently being
+	! parsed, and applies the rules of scope recursively so that contents of
+	! see-through objects are also in scope, as is anything added to scope.
+	! No return value
+];
 
-    ! If the item has been added to scope by something, it's first necessary
-    ! for that something to be touchable.
-    !"TODO: ObjectIsUntouchable";
+[ TestScope p_obj p_actor;
+	! Tests whether the object obj is in scope to the given actor. If no
+	! actor is given, the actor is assumed to be the player.
+	! The routine returns true or false.
+];
+
+! Determines whether any solid barrier, that is, any container that is not
+! open, lies between the player and obj. If flag is true, this routine never
+! prints anything; otherwise it prints a message like “You can't, because
+! … is in the way.” if any barrier is found.
+! The routine returns   true if a barrier is found, false if not.
+!
+! (not DM) If flag2 is set, also apply Take/Remove restrictions.
+[ ObjectIsUntouchable p_item p_flag p_flag2 _ancestor _i;
     rfalse;
 ];
 
