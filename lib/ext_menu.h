28eac92f06c26808101bd759174bbba0f4eb5d43
Johan Berntsson <johanberntsson@users.noreply.github.com>
2020-05-28 20:35:53 +0900

    help with ext_menu.h added to testbench

---
+++ lib/ext_menu.h
@@ -0,0 +1,44 @@
+Global menu_item;
+Global item_width;
+Global item_name;
+Global menu_nesting;
+
+#IfV3;
+[ DoMenu menu_choices EntryR ChoiceR lines main_title i j;
+	menu_nesting++;
+.LKRD;
+	menu_item = 0;
+	lines = indirect(EntryR);
+	main_title = item_name;
+
+	print "--- "; print (string) main_title; print " ---^^";
+
+	if (menu_choices ofclass Routine) menu_choices.call();
+	else                              print (string) menu_choices;
+
+	for (::) {
+		print "^Type a number from 1 to ", lines, ", 0 to redisplay or press ENTER.";
+		print "> ";
+
+       _ReadPlayerInput(true);
+		j = parse_array->1; ! number of words
+		i = parse_array-->1;
+		if (j == 0) { ! || (i == QUIT1__WD or QUIT2__WD)) {
+			menu_nesting--; if (menu_nesting > 0) rfalse;
+			if (deadflag == 0) <<Look>>;
+			rfalse;
+		}
+		i = TryNumber(1);
+		if (i == 0) jump LKRD;
+		if (i < 1 || i > lines) continue;
+		menu_item = i;
+		j = indirect(EntryR);
+		print "^--- "; print (string) item_name; print " ---^^";
+		j = indirect(ChoiceR);
+		if (j == 2) jump LKRD;
+		if (j == 3) rfalse;
+	}
+];
+#IfNot;
+#EndIf;
+
