f6fc5fffbc57a8c3cb87b336cd9939482d77d108
fredrikramsberg <fredrik.ramsberg@ibinder.com>
2020-08-29 18:19:16 +0200

    Updated commands for test1.inf

--- tests/test1.cmd
+++ tests/test1.cmd
@@ -1,6 +1,20 @@
 transcript
 
-look
+get ball
+red
+get all balls
+get cubes
+drop black cube
+drop cubes
+drop cubes
+get black ball
+s
+put all balls but the red one in the red box
+red ball
+put all in transparent
+get all from transparent
+drop red ball
+get it
+get all from red box
 quit
 y
-
