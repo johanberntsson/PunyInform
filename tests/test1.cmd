312981197c38729c856f277cd6d530765d21e392
fredrikramsberg <fredrik.ramsberg@ibinder.com>
2020-09-02 11:02:59 +0200

    Updated test1.cmd

--- tests/test1.cmd
+++ tests/test1.cmd
@@ -10,12 +10,9 @@ drop cubes
 get black ball
 s
 put all balls but the red one in the red box
-red ball
 put all in transparent
 get all from transparent
 drop red ball
 get it
 get all from red box
 quit
-y
-
