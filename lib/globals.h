73f95cdacec90df6108e5bcd4bd3dcee705f38d8
Johan Berntsson <johanberntsson@users.noreply.github.com>
2020-11-04 20:46:46 +0900

    Updated manual and version to 1.7

--- lib/globals.h
+++ lib/globals.h
@@ -3,7 +3,7 @@
 System_file;
 
 Constant PUNYINFORM_MAJOR_VERSION = 1;
-Constant PUNYINFORM_MINOR_VERSION = 6;
+Constant PUNYINFORM_MINOR_VERSION = 7;
 
 #IfnDef CUSTOM_ABBREVIATIONS;
 Abbreviate ". ";
