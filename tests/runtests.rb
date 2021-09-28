f4171f1bc002c4676b46695a2b9a7ee15f2399ae
Johan Berntsson <johan.mayumi@gmail.com>
2021-09-28 15:33:59 +0900

    runtests now checks for warnings too

--- tests/runtests.rb
+++ tests/runtests.rb
@@ -37,7 +37,7 @@ def runtest(filename, version, inform_args)
     begin
         result = %x[#{inform_cmd}]
         File.open(compileroutput_file, 'w') { |file| file.write(result) }
-        if result.include? "(no output)" then
+        if result.include?("(no output)") || result.include?("Warning") then
             puts result
             raise Errno::ENOENT
         end
