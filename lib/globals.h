a7acc7b961c3804c2d4dc833d658e6059f0eedba
Johan Berntsson <johan.mayumi@gmail.com>
2021-11-11 17:00:15 +0900

    New version number

--- lib/globals.h
+++ lib/globals.h
@@ -3,7 +3,7 @@
 System_file;
 
 Constant PUNYINFORM_MAJOR_VERSION = 3;
-Constant PUNYINFORM_MINOR_VERSION = 1;
+Constant PUNYINFORM_MINOR_VERSION = 2;
 
 #Ifndef VN_1635;
 Message fatalerror "*** The PunyInform library needs Inform v6.35 or later to work ***";
