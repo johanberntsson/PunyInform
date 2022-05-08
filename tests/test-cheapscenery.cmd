3643b9f7f3dd60ad887edfba13792992542211d0
Fredrik Ramsberg <fredrik.ramsberg@gmail.com>
2022-05-08 12:20:04 +0200

    Added test for cheap scenery.

---
+++ tests/test-cheapscenery.cmd
@@ -0,0 +1,23 @@
+transcript
+
+x book
+push books
+burn book books
+get books book
+x wall
+push walls
+x ceiling fan
+touch it
+turn it
+x atmosphere
+push atmosphere
+get air
+turn library
+x it
+x shelf
+x air
+x thin air
+x big library
+x box
+quit
+yes
