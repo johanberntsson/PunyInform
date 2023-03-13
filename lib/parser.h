e0f8f49527324e1f23abfd5facb0a7fa03377203
Johan Berntsson <johan.mayumi@gmail.com>
2023-03-13 18:27:12 +0900

    Fixed a bug in _PrintPatternSyntax to handle patterns with routine calls

--- lib/parser.h
+++ lib/parser.h
@@ -120,7 +120,7 @@ System_file;
 					}
 				} else print (name) second;
 			} else {
-				if(noun ~= 0) {
+				if(noun ~= 0 && metaclass(noun) ~= ROUTINE) {
 					p_noun = 0; ! avoid repeat (we don't need p_noun anymore)
 					if(parser_all_found) print "all"; else print (name) noun;
 				} else if(_type == TT_OBJECT && _token->2 == CREATURE_OBJECT) {
