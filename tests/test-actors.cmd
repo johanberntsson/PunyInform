384a66985b73d826cfeae1382168461915c133ae
Johan Berntsson <johan.mayumi@gmail.com>
2024-05-13 11:34:46 +0900

    Added new test test-actors

---
+++ tests/test-actors.cmd
@@ -0,0 +1,24 @@
+transcript
+
+
+again
+take all
+drop all
+take child
+take george
+ask george to look
+ask george about me
+ask george to do something
+eat apple
+eat apple
+eat george
+unlock door with key
+red
+drop all
+take blue key
+unlock door with key
+george, look
+child, look
+quit
+yes
+
