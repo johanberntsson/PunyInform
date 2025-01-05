751eb6413889fab4f3a89286f3a14630defda8af
Fredrik Ramsberg <fredrik.ramsberg@gmail.com>
2025-01-06 00:11:47 +0100

    - talk_menu extension: A relative reference to a topic to be activated can now be negative as well as positive. If it's negative, the number has to preceded by TM_ACTIVATE, and the number has to be in parentheses. E.g. TM_ACTIVATE (-1) - talk_menu extension: A topic can now inactivate another topic, by specifying TM_INACTIVATE followed by either a positive or negative relative reference (e.g. (-1) means the previous topic), or an absolute ID 300-600.

--- lib/ext_talk_menu.h
+++ lib/ext_talk_menu.h
@@ -7,7 +7,7 @@ System_file;
 !
 ! A talk topic has the following form:
 !
-! STATUS [ID]* TOPIC PLAYERSAYS NPCSAYS [FLAGREF|UNLOCKREF|ROUTINE|STRING]*
+! STATUS [ID]* TOPIC PLAYERSAYS NPCSAYS [FLAGREF|ACTIVATEREF|INACTIVATEREF|ROUTINE|STRING]*
 !
 ! or:
 !
@@ -18,10 +18,11 @@ System_file;
 !
 ! STATUS is either 0 ( = TM_INACTIVE = not active) or
 !   30 ( = TM_ACTIVE  = active) or 31 ( = TM_STALE, has been used).
-! ID is a number (300-600) which can be used as a reference to activate
-!   a topic in code or using an UNLOCKREF in talk_array. Note that IDs are
-!   local to the NPC - two different NPCs can use the same ID for different
-!   topics without risk of confusion.
+! ID is a number (300-600) which can be used as a reference to 
+!   activate/inactivate a topic in code or using an ACTIVATEREF or 
+!   INACTIVATEREF in a talk array. Note that IDs are local to the NPC - two 
+!   different NPCs can use the same ID for different topics without risk of 
+!   confusion.
 ! TOPIC is a string or routine for the topic name
 ! PLAYERSAYS can be on any of these forms (ROUTSTR means a routine or string):
 !    * ROUTSTR
@@ -35,12 +36,21 @@ System_file;
 !    it the value TM_NO_LINE
 ! FLAGREF is a number 32-299 for a flag to be set (In order to use this,
 !    you must include ext_flags.h before including ext_talk_menu.h)
-! UNLOCKREF is either a topic ID (300-600) or a relative reference to a topic
-!    (1 to 20) that is activated by this topic. 1 means the next topic,
-!    2 the topic after that etc. The target topic has to have status
-!    TM_INACTIVE (= 0) or TM_ACTIVE (= 30) for this to work. When a topic
-!    is chosen in a conversation, it is set to status TM_STALE, and the only
-!    way to activate it when it's stale is to call ReActivateTopic.
+! ACTIVATEREF is the keyword TM_ACTIVATE followed be either a 
+!    topic ID (300-600) or a relative reference to a topic (1 to 20, or 
+!    -1 to -20) that is activated by this topic. 1 means the next topic,
+!    2 the topic after that etc. The keyword TM_ACTIVATE can be skipped, 
+!    unless the reference number is negative. Note that a negative number has
+!    to be enclosed in parentheses. When a topic is picked by the player, 
+!    it is set to status TM_STALE, and the only way to activate it when it's 
+!    stale is to call ReActivateTopic.
+! INACTIVATEREF is the keyword TM_INACTIVATE followed be either a 
+!    topic ID (300-600) or a relative reference to a topic (1 to 20, or 
+!    -1 to -20) that is activated by this topic. 1 means the next topic, 2 the
+!    topic after that etc. Note that a negative number has to be enclosed in 
+!    parentheses. When a topic is picked by the player, it is set to status
+!    TM_STALE, and the only way to activate it when it's stale is to call 
+!    ReActivateTopic.
 ! ROUTINE is a routine to be run. In this routine, the global variable
 !    current_talker refers to the NPC being talked to.
 ! STRING is a string to be printed.
@@ -55,10 +65,11 @@ System_file;
 !    that routine returns true, the topic list held in the sub-array specified 
 !    will be added. A few restrictions apply: 
 !        (1) The sub-array may not contain a TM_MAYBE_ADD_LIST token.
-!        (2) a relative UNLOCKREF in the base array may not refer to a topic 
-!            within the sub-array or after the TM_MAYBE_ADD_LIST token.
-!        (3) a relative UNLOCKREF in the sub-array may not refer to a topic 
-!            beyond the end of the sub-array.
+!        (2) a relative ACTIVATEREF or INACTIVATEREF in the base array may not
+!            refer to a topic within a sub-array or past a TM_MAYBE_ADD_LIST
+!            token.
+!        (3) a relative ACTIVATEREF or INACTIVATEREF in a sub-array may only
+!            refer to a topic within the same sub-array.
 ! FLAGREF is a flag number, normally between 32 and 299. If the flag is set,
 !    the sub-array is included.
 ! ROUTINE is a routine to decide if the sub-array should be included. It may
@@ -73,11 +84,11 @@ System_file;
 ! [ EnoughTalk; talk_menu_talking = false; ];
 !
 ! Array talk_array_linda -->
-! 0 300 "Herself" "Tell me more about yourself!" "I'm just an average girl."
-! 30 "Weather" "How do you like the weather?" "It's too hot for me." 1 300
-! 0 "Heat" "Say, don't you like hot weather?" TM_NO_LINE EnoughTalk
+!   0 300 "Herself" "Tell me more about yourself!" "I'm just an average girl."
+!   30 "Weather" "How do you like the weather?" "It's too hot for me." 1 300
+!   0 "Heat" "Say, don't you like hot weather?" TM_NO_LINE EnoughTalk
 !          "Linda looks upset and turns away."
-! TM_END;
+!   TM_END;
 !
 ! Object Linda "Linda"
 !   with talk_array talk_array_linda,
@@ -100,14 +111,14 @@ System_file;
 ! 3. In conjuction with this, you can also use TM_FIRST_ID to define where
 !    flags end and topic IDs begin.
 !
-! Apart from activating topics using UNLOCKREFs in the talk_array, you can also
-! use these routines:
+! Apart from activating topics using ACTIVATEREFs and INACTIVATEREFs in a 
+! talk_array, you can also use these routines:
 ! ActivateTopic(NPC, topic) => true for success, false for fail
-!   Activates the topic if it's currently inactive or active (not stale!).
+!   Activates the topic, unless it's stale.
 ! ReActivateTopic(NPC, topic) => true for success, false for fail
 !   Activates the topic, regardless of its status.
 ! InactivateTopic(NPC, topic) => true for success, false for fail
-!   Inactivates the topic if it's currently inactive or active (not stale!).
+!   Inactivates the topic, unless it's stale.
 ! ReInactivateTopic(NPC, topic) => true for success, false for fail
 !   Inactivates the topic, regardless of its status.
 ! GetTopicStatus(NPC, topic) => topic status (TM_INACTIVE, TM_ACTIVE, TM_STALE)
@@ -222,10 +233,12 @@ Constant TM_MSG_PAGE_OPTION_SHORT "[N]ext"; ! This + TM_MSG_EXIT_OPTION_SHORT sh
 #Endif;
 
 Constant TM_INACTIVE 0;
+Constant TM_ACTIVATE 27;
+Constant TM_INACTIVATE 28;
 Constant TM_MAYBE_ADD_LIST 29;
 Constant TM_ACTIVE 30;
 Constant TM_STALE 31;
-Constant TM_END -1;
+Constant TM_END 26;
 
 Constant TM_NO_LINE 1; ! Can be used instead of a player's line or an actor's line ONLY!
 Constant TM_ADD_BEFORE 2; ! Can be used directly after subject
@@ -280,6 +293,9 @@ Global talk_menu_multi_mode = true;
 	_index--;
 	while(true) {
 		_index++;
+		if(p_topic < 0 && p_topic >= -21) {
+			_index = _index - 2;
+		}
 		_val = p_array-->_index;
 		if(_val == TM_END) {
 			if(_find_topic < 29) rfalse; ! Signal failure, as the topic was not found
@@ -315,7 +331,7 @@ Global talk_menu_multi_mode = true;
 ._tm_found_topic;
 					if(p_value == 1)
 						return p_array-->_index;
-					if(_val ~= TM_STALE || p_topic < 0) {
+					if(_val ~= TM_STALE || p_topic < -21) {
 						p_array-->_index = p_value;
 					}
 					if(_find_topic < 29 || talk_menu_multi_mode == false) {
@@ -649,8 +665,31 @@ Array _TMLines --> 10;
 		}
 
 		if(_val > 0) {
+			if(_val == TM_INACTIVATE) {
+				! Next value is an absolute or relative reference to a topic to inactivate
+				_j++;
+				_val = _array-->_j;
+				! TODO: Check valid range here!
+				if(_val >= TM_FIRST_ID) {
+					! An absolute ID for a topic to inactivate
+					InActivateTopic(p_npc, _val);
+				} else {
+					! A relative ID for a topic to inacivate
+					if(_val < 0) 
+						_val--;
+					InActivateTopic(p_npc, _val, _array + _j + _j);
+				}
+				continue;
+			}
+			if(_val == TM_ACTIVATE) {
+				_j++;
+				_val = _array-->_j;
+				! TODO: Check valid range here!
+			}
 			if(_val < 30) {
 				! A relative reference to a topic to activate
+				if(_val < 0) 
+					_val--;
 				ActivateTopic(p_npc, _val, _array + _j + _j);
 				continue;
 			}
