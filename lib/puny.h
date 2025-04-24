3ca0049555429259cded7cc897831c733de6e72f
Johan Berntsson <johan.mayumi@gmail.com>
2025-04-24 15:29:00 +0900

    Fixed indentation

--- lib/puny.h
+++ lib/puny.h
@@ -2444,7 +2444,7 @@ Object thedark "Darkness"
 			_ReadPlayerInput();
 			if(buffer->1 == 42) { ! asterisk
 				if(transcript_mode) PrintMsg(MSG_COMMENT_TRANSCRIPT);
-                else PrintMsg(MSG_COMMENT_NO_TRANSCRIPT);
+				else PrintMsg(MSG_COMMENT_NO_TRANSCRIPT);
 				jump _abort_current_input;
 			}
 			_disallow_complex_again = false;
