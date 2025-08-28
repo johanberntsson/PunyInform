3026dcceac0c5ada96f5632a621e5894d196c3ae
framsberg <fredrik.ramsberg@gmail.com>
2025-08-28 18:40:37 +0200

    Bumped version# to 6.0.1

--- lib/globals.h
+++ lib/globals.h
@@ -4,8 +4,8 @@ System_file;
 
 Constant PUNYINFORM_MAJOR_VERSION = 6;
 Constant PUNYINFORM_MINOR_VERSION = 0;
-Constant PUNYINFORM_PATCH_VERSION = 0; ! Usually 0 (if zero, it is not printed in banner)
-Constant PUNYINFORM_VERSION_SUFFIX = "dev"; ! Comment out if none
+Constant PUNYINFORM_PATCH_VERSION = 1; ! Usually 0 (if zero, it is not printed in banner)
+!Constant PUNYINFORM_VERSION_SUFFIX = "dev"; ! Comment out if none
 
 #Ifndef VN_1643;
 Message fatalerror "*** The PunyInform library needs Inform v6.43 or later to work ***";
