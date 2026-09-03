d091ce2650fe4e7bacbee9693fec535d65e6070c
Johan Berntsson <johan.mayumi@gmail.com>
2026-09-03 17:43:15 +0900

    Added test-random

---
+++ tests/test-random.cmd
@@ -0,0 +1,15 @@
+transcript
+
+random seed 2
+look
+foo for 1
+foo for 1 unit
+blob
+blob me to 4
+blob me to 5 minute
+blob me to 0 minute
+blob me to 10000 minute
+blob me to 10001 minute
+quit
+yes
+
